fc300eb167674f667df9185eba127566d354328f
lucasmteix <118843057+lucasmteix@users.noreply.github.com>
2024-10-31 20:10:13 -0300

    Bateria de testes uniforme

--- src/Cozinha.java
+++ src/Cozinha.java
@@ -38,8 +38,8 @@ public class Cozinha {
         System.out.print("Entre com o número de cozinheiros: ");
         quantidadeCozinheiros = leitor.nextInt();
 
-        System.out.print("Entre com o número de pratos: ");
-        quantidadePratos =  leitor.nextInt();
+        /*System.out.print("Entre com o número de pratos: ");
+        quantidadePratos =  leitor.nextInt();*/
 
         long comeco = System.nanoTime();
 
@@ -51,12 +51,22 @@ public class Cozinha {
         }
 
         //Preenchendo o array de pratos
-        for(int i = 1; i <= quantidadePratos; i++){
+        /*for(int i = 1; i <= quantidadePratos; i++){
 
             novoPrato = new Prato(nomesPratos[gerador.nextInt(nomesPratos.length)] + " (" +
                     Integer.toString(i) + ")", gerador.nextInt(11));
             pratos.add(novoPrato);
-        }
+        }*/
+        pratos.add(new Prato("Feijoada", 8));
+        pratos.add(new Prato("Arroz com Feijão", 4));
+        pratos.add(new Prato("Pizza Margherita", 5));
+        pratos.add(new Prato("Hambúrguer", 6));
+        pratos.add(new Prato("Pastel de Carne", 3));
+        pratos.add(new Prato("Macarrão à Bolonhesa", 7));
+        pratos.add(new Prato("Lasanha de Queijo", 9));
+        pratos.add(new Prato("Sushi", 6));
+        pratos.add(new Prato("Bife à Parmegiana", 8));
+        pratos.add(new Prato("Churrasco", 10));
 
         //Iniciando os cozinheiros
         for(Cozinheiro cozinheiro : cozinheiros){
